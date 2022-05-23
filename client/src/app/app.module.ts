765c9af803350db030d7b2f0ecc894a5c1b17653
Stephen Hanjaya <stephenhanjaya@gmail.com>
2022-05-22 18:32:38 -0700

    added functional Notify Me button for item costs more than a certain amount, also share buttons for each item.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    // this one being passed to <router-outlet></router-outlet> in app.component.html
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
